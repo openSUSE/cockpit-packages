5b6214d4b0659ed0d16734bd3a9f6cf872112e9c
Miika Alikirri <miika.alikirri@suse.com>
2026-06-24 11:29:03 +0300

    Try waiting for zypper lock to be released when getting packages
    Cockpit-tukit and other programs that call zypper can cause a race
    condition where packagekit is not able to use zypper due to zypper being
    locked. Trying couple of times and waiting might solve that issue.

--- src/backend/packagekit.ts
+++ src/backend/packagekit.ts
@@ -2,23 +2,55 @@ import * as PK from "packagekit.js";
 import { Backend, Package } from "./backend";
 // import { getPackageManager } from "packagemanager";
 import { PackageKitManager } from "_internal/packagekit";
+import { TransactionError } from "packagekit";
+
+// PackageKit error sometimes caused when zypper is used by another program.
+// Often happens when cockpit-tukit calls zypper, which takes the zypper lock.
+const PK_ERROR_ENUM_FAILED_INITIALIZATION = 22;
+
+const sleep = (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const tryWaitZyppLock = async (cb: () => Promise<void>) => {
+    const maxCount = 5;
+    for (let count = 1; count <= maxCount; count++) {
+        try {
+            await cb();
+            return Promise.resolve();
+        } catch (e) {
+            const ex = e as Error;
+            if (ex instanceof TransactionError) {
+                // XXX: might be worth it to check if the error details contain "zypper"
+                if (count < maxCount && ex.code === PK_ERROR_ENUM_FAILED_INITIALIZATION) {
+                    await sleep(10 * 1000);
+                } else {
+                    return Promise.reject(ex);
+                }
+            }
+        }
+    }
+
+    Promise.reject(new Error("Unkown error happened while waiting for zypper lock"));
+};
 
 export class PackageKit implements Backend {
     getInstalled(): Promise<Package[]> {
         return new Promise((resolve, reject) => {
             const installed: Package[] = [];
-            // XXX: This doesn't work if tukitd is installed / running
-            PK.cancellableTransaction("GetPackages", [PK.Enum.FILTER_INSTALLED], null, {
-                Package: (info: typeof PK.Enum, packageId: string, summary: string) => {
-                    const fields = packageId.split(";");
-                    installed.push({
-                        summary,
-                        id: packageId,
-                        name: fields[0],
-                        version: fields[1],
-                    });
-                },
-            }).then(() => {
+            tryWaitZyppLock(() =>
+                PK.cancellableTransaction("GetPackages", [PK.Enum.FILTER_INSTALLED], null, {
+                    Package: (info: typeof PK.Enum, packageId: string, summary: string) => {
+                        const fields = packageId.split(";");
+                        installed.push({
+                            summary,
+                            id: packageId,
+                            name: fields[0],
+                            version: fields[1],
+                        });
+                    },
+                })
+            ).then(() => {
                 resolve(installed);
             }).catch(ex => {
                 console.log(ex);
@@ -30,17 +62,19 @@ export class PackageKit implements Backend {
     searchPackages(pkgName: string): Promise<Package[]> {
         return new Promise((resolve, reject) => {
             const found: Package[] = [];
-            PK.cancellableTransaction("SearchNames", [0, [pkgName]], null, {
-                Package: (info: typeof PK.Enum, packageId: string, summary: string) => {
-                    const fields = packageId.split(";");
-                    found.push({
-                        summary,
-                        id: packageId,
-                        name: fields[0],
-                        version: fields[1],
-                    });
-                },
-            }).then(() => {
+            tryWaitZyppLock(() =>
+                PK.cancellableTransaction("SearchNames", [0, [pkgName]], null, {
+                    Package: (info: typeof PK.Enum, packageId: string, summary: string) => {
+                        const fields = packageId.split(";");
+                        found.push({
+                            summary,
+                            id: packageId,
+                            name: fields[0],
+                            version: fields[1],
+                        });
+                    },
+                })
+            ).then(() => {
                 resolve(found);
             }).catch(ex => {
                 console.log(ex);
